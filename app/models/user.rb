fbee5d84b0027fce8c396c79a3760816e635723d
black-adm <mattheuscontato17@gmail.com>
2023-11-26 12:59:15 -0300

    feat: add and remove permissions services

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
 class User < ApplicationRecord
   has_many :permissions
-
-  def set_permission(permission_name)
-    permissions.create(name: permission_name)
-    activate!("because #{permission_name} was created!")
-  end
-
-  def remove_permission(permission_name)
-    permissions.find_by(name: permission_name).delete
-    desactivate!("because #{permission_name} was removed!")
-  end
 end
